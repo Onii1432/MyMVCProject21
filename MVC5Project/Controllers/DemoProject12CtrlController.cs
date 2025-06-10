4c3bcc7188c24a844654e8b42e7e52b733cdc476
sneha_gharat <42944127+snehagharat@users.noreply.github.com>
2025-06-10 16:14:45 +0530

    Add one Ctrl 'DemoProject12Ctrl'

---
+++ MVC5Project/Controllers/DemoProject12CtrlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Project.Controllers
+{
+    public class DemoProject12CtrlController : Controller
+    {
+        // GET: DemoProject12Ctrl
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
